cf685bb646b4894775346dd8cd35da129b1e2ea3
tron1c <alaali32@gmail.com>
2025-02-10 16:58:20 +0300

    first test push

---
+++ app.js
@@ -0,0 +1,96 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+const isAdult = function(age)  
+{
+if (age >=18 )  {
+return 'adult' ;
+}
+else {  return 'Minor' ;
+}
+}
+console.log('Exercise 2 Result:', isAdult(21));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+function isCharAVowel(char) {
+ 
+  
+  // List of vowels
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+ 
+  for (let i = 0; i < vowels.length; i++) {
+      if (char === vowels[i]) {
+          return true;
+      }
+  }
+  
+ 
+  return false;
+}
+
+
+console.log('Exercise 3 Result:', isCharAVowel("a")); 
+
+//
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+function generateEmail(name, domain) {
+  // Combine the name and domain with an '@' symbol
+  return name + "@" + domain;
+}
+
+// Test the function
+console.log('Exercise 4 Result:', generateEmail("Ahmed", "AhmedAlaali.com")); // Expected output: 'johnsmith@example.com'
+
